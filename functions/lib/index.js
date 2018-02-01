f1a736ab8d4bdf5ffd03ab9137d6735a2176aafd
Alfredo Luco <aluco100@gmail.com>
2018-02-01 18:26:58 -0300

    push on server

--- functions/lib/index.js
+++ functions/lib/index.js
@@ -4,6 +4,8 @@ const functions = require("firebase-functions");
 // Start writing Firebase Functions
 // https://firebase.google.com/docs/functions/typescript
 const cors = require('cors')({ origin: true });
+const admin = require('firebase-admin');
+admin.initializeApp(functions.config().firebase);
 exports.helloWorld = functions.https.onRequest((request, response) => {
     console.log(Date.now());
     response.send({ now: Date.now() / 1000 | 0 });
@@ -11,6 +13,14 @@ exports.helloWorld = functions.https.onRequest((request, response) => {
 exports.getNowFromServer = functions.https.onRequest((request, response) => {
     cors(request, response, () => {
         response.status(200).send({ now: Date.now() / 1000 | 0 });
+    });
+});
+exports.insertStartedAt = functions.https.onRequest((request, response) => {
+    cors(request, response, () => {
+        let message = request.body;
+        message.createdAt = Date.now() / 1000 | 0;
+        admin.database().ref('/chat').push(message);
+        response.status(200).send(message);
     });
 });
 //# sourceMappingURL=index.js.map
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -3,6 +3,8 @@ import * as functions from 'firebase-functions';
 // Start writing Firebase Functions
 // https://firebase.google.com/docs/functions/typescript
 const cors = require('cors')({origin: true});
+const admin = require('firebase-admin');
+admin.initializeApp(functions.config().firebase);
 
 export const helloWorld = functions.https.onRequest((request, response) => {
   console.log(Date.now());
@@ -15,3 +17,12 @@ export const getNowFromServer = functions.https.onRequest((request, response) =>
     response.status(200).send({now : Date.now() / 1000 | 0});
   })
 });
+
+export const insertStartedAt = functions.https.onRequest((request, response) => {
+  cors(request,response,()=>{
+    let message = request.body;
+    message.createdAt = Date.now() / 1000 | 0;
+    admin.database().ref('/chat').push(message);
+    response.status(200).send(message);
+  });
+})
